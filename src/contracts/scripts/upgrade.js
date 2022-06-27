d7da477260a5523baf48295bf960d4a226dc9115
tatz210588 <tathagat.saha@gmail.com>
2022-06-28 04:57:18 +0530

    .

--- src/contracts/scripts/upgrade.js
+++ src/contracts/scripts/upgrade.js
@@ -3,7 +3,7 @@ const { ethers, upgrades } = require("hardhat")
 async function main() {
     const pogPlay = await ethers.getContractFactory("POGPlay")
     let proxy = await upgrades.upgradeProxy("0xD07aa7216962C5f90D989A0c0435Dc8926C18c04", pogPlay) //Rinkeby (proxy address)
-    //let proxy = await upgrades.upgradeProxy("0x96dbc0D0DD46EAf516a5f04B3A2864633BA78FD4", pogPlay) //Bitgert testnet (proxy address)
+    //let proxy = await upgrades.upgradeProxy("0x90179ba681708dC36C38828153130D5B7836b7D5", pogPlay) //Bitgert testnet (proxy address)
     console.log("POGPlay smart contract has been successfully upgraded.")
 }
 
